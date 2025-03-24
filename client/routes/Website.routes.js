1d9ebd6216afe759c74acf32f1beb5cabfcb3082
rocket125 <149022535+rocket125@users.noreply.github.com>
2025-03-24 15:50:35 -0400

    Add webpage navigation when connected to server

--- client/routes/Website.routes.js
+++ client/routes/Website.routes.js
@@ -1,4 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const path = require('path');
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.resolve('client/public/index.html'))
+});
+
+router.get('/login.html', (req, res) => {
+    res.sendFile(path.resolve('client/public/login.html'))
+});
+
+router.get('/signup.html', (req, res) => {
+    res.sendFile(path.resolve('client/public/signup.html'))
+});
 
 module.exports = router;
--- client/server.js
+++ client/server.js
@@ -3,16 +3,14 @@ const app = express();
 const port = 3000;
 const webRoutes = require("./routes/Website.routes");
 
-/*
 app.get('/', (req, res) => {
   res.sendFile('public/index.html', {root: __dirname})
 });
 
+app.use('/webpage/', webRoutes);
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
- */
-
-app.use(`/`, route);
 
 module.exports = app;
