2147381235f36f5a454cd98858fc64d96885b643
ender <enderdarogue@gmail.com>
2025-03-24 11:30:47 -0400

    Add website route to traverse between webpages

--- client/server.js
+++ client/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const webRoutes = require("./routes/Website.routes");
 
+/*
 app.get('/', (req, res) => {
   res.sendFile('public/index.html', {root: __dirname})
 });
@@ -9,5 +11,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
+ */
+
+app.use(`/`, route);
 
 module.exports = app;
