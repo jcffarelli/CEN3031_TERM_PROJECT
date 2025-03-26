5ab3d6c1d6fa0916482f84548706a1d5a137d774
rocket125 <149022535+rocket125@users.noreply.github.com>
2025-03-26 15:26:14 -0400

    Refactor file to match with master for consistent merge

--- src/server/routes/Website.routes.js
+++ src/server/routes/Website.routes.js
@@ -5,15 +5,15 @@ const router = express.Router();
 const path = require('path');
 
 app.get('/index', (req, res) => {
-    res.sendFile(path.resolve('client/public/index.html'))
+    res.sendFile(path.resolve('server/public/index.html'))
 });
 
 router.get('/login.html', (req, res) => {
-    res.sendFile(path.resolve('client/public/login.html'))
+    res.sendFile(path.resolve('server/public/login.html'))
 });
 
 router.get('/signup.html', (req, res) => {
-    res.sendFile(path.resolve('client/public/signup.html'))
+    res.sendFile(path.resolve('server/public/signup.html'))
 });
 
 module.exports = router;
