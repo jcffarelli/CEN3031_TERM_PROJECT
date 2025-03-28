429ff490f346b0158d73fa43d558c2353c8e2505
rocket125 <149022535+rocket125@users.noreply.github.com>
2025-03-28 12:40:07 -0400

    Fix home landing page not directing to registration page

--- src/server/server.js
+++ src/server/server.js
@@ -19,6 +19,10 @@ app.get('/', (req, res) => {
   res.sendFile('home.html', { root: dirname } );
 });
 
+app.get('/registration.html', (req, res) => {
+	res.sendFile('registration.html', { root: dirname } );
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
